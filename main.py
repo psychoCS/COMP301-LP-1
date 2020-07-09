0f06136abb3fb1631c55d7ac085ee4a659babc6f
psychoCS <thiagoluizb@hotmail.com>
2020-07-09 15:47:12 -0400

    Added Distance

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from student import Student
+from vector2d import Vector2D
 
 tom = Student("Tom", 30, 301110875)
 
 tom.saysHello()
 tom.studies()
+
+position1 = Vector2D(20.0, 30.1)
+position2 = Vector2D(10.0, 10.0) 
+position3 = Vector2D(100.3, 87.4)
+
+position3 = position1 + position2
+
+print(position1)
+
+position4 = position3 - position2
+
+print(position4)
--- vector2d.py
+++ vector2d.py
@@ -3,3 +3,35 @@ class Vector2D(object):
     def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
+
+    #overload the binary +
+    def __add__(self, rhs):
+        Xs = self.x + rhs.x
+        Ys = self.y + rhs.y
+
+        self.x = Xs
+        self.y = Ys
+
+        return self.x, self.y
+
+    #overload the -
+    def __sub__(self, rhs):
+        diffXs = self.x - rhs.x
+        diffYs = self.y - rhs.y
+        
+        self.x = diffXs
+        self.y = diffYs
+
+        return self.x, self.y
+
+    #utility function for distance
+    def Distance(vector1, vector2):
+        diffXs = vector2.x - vector1.x
+        diffYs = vector2.y - vector1.y
+        return (diffXs * diffXs + diffYs * diffYs) ** 0.5
+        
+
+
+
+
+        
